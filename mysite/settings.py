acaf1acfe12fdca900733bf6102aacb09d401677
Ssuny <kyn03136@naver.com>
2018-11-01 18:05:29 +0900

    setting file change

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,3 +131,5 @@ LOGIN_REDIRECT_URL = '/'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
